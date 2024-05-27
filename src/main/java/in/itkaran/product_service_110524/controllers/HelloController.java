d0ba76ea6aa7379d4467fbc2316cd12c8ed6c285
Kumar Karan <contact.karan.ai@gmail.com>
2024-05-27 14:51:44 +0530

    Add colors to the welcome text

--- src/main/java/in/itkaran/product_service_110524/controllers/HelloController.java
+++ src/main/java/in/itkaran/product_service_110524/controllers/HelloController.java
@@ -14,22 +14,27 @@ public class HelloController {
         str+="</h1>";
         str+="<br>";
         str+="<h2>";
+        str+="<mark>";
         str+="This is your last chance.";
         str+="<br>";
         str+="After this, there is no turning back.";
+        str+="</mark>";
         str+="<br>";
+        str+="<p style=\"color: blue;\">";
         str+="You take the blue pill - the story ends,";
         str+="<br>";
         str+="You wake up in your bed";
         str+="<br>";
         str+="And believe whatever you want to believe.";
-        str+="<br>";
+        str+="</p>";
+        str+="<p style=\"color: red;\">";
         str+="You take the red pill - ";
         str+="<br>";
         str+="You stay in Wonderland";
         str+="<br>";
         str+="And I show you how deep the rabbit hole goes.";
         str+="<br>";
+        str+="</p>";
         str+="</h2>";
         return new ResponseEntity<>(str, HttpStatus.OK);
     }
